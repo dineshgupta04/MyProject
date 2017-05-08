1e31f3bf742e8efcb92da732d021589d6589da56
dineshgupta04 <dineshlk.gupta@gmail.com>
2017-05-08 12:58:13 +0530

    Added file

---
+++ Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+	final static int a=5;
+	public  void count(){
+		System.out.println(a);
+		
+	}
+
+	public static void main(String[] args){
+		Test a=new Test();
+		a.count();
+		a.count();
+	}
+
+}
